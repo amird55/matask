cfde1dd58d98b3b6e56530a91bf17baec2e9c563
amird55 <amir.dgn@outlook.com>
2025-01-24 12:53:08 +0200

    הצגת שורה ראשונה באופן דינמי

--- js/main.js
+++ js/main.js
@@ -4,7 +4,25 @@ async function GetMileStones(){
     let response=await fetch(url);
     let reply=await response.json();
     mStones = reply.data;
+    console.log(mStones);
+    CreateTableHeader();
 }
-function CreateTableHeader(){
+function CreateTableHeader() {
+    let s = "";
+    s+="<tr>";
+    s+="    <th>המטלה</th>";
+    s+="    <th>אחראי</th>";
+    s+="    <th>תאריך יעד</th>";
+    s+="    <th>אחוז התקדמות</th>";
+    s+="    <th>קטגוריה</th>";
+    s+="    <th>תיאור</th>";
+    for(let row of mStones) {
+        s += "<th>";
+        s += `${row.name}`;
+        s += "</th>";
+    }
+    s+="</tr>";
+    document.getElementById("mainHeader").innerHTML = s;
+}
 
-}
+GetMileStones();
